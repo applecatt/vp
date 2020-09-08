4496fc04838df0aa02c9666728ff708794743854
Harry Loog <applecat@users.noreply.github.com>
2020-09-08 11:31:19 +0300

    kodutöö

--- home.php
+++ home.php
@@ -1,13 +1,36 @@
+<?php
+  $username = "Harry O'Loog";
+  $fulltimenow = date("d.m.Y H:i:s");
+  $hournow = date("H");
+  $partofday = "lihtsalt aeg";
+  if($hournow < 6){
+		$partofday = "uneaeg";
+  }//enne kuut
+  if($hournow >= 8 and $hournow <= 18){
+	  $partofday = "õppimise aeg";
+  }
+  
+  //vaatame semestri kulgemist
+  $semesterstart = new DateTime("2020-8-31");
+  $semesterend = new DateTime("2020-12-13");
+  $semesterduration = $semesterstart->diff($semesterend);
+  $semesterdurationdays = $semesterduration->format("%r%a); //%a - päevade arv
+  $today = new DateTime("now");
+  $dayselapsed = $semesterstart->diff($today);
+  
+?>
 <!DOCTYPE html>
 <html lang="et">
 <head>
   <meta charset="utf-8">
-  <title>Harry Loog programmeerib veebi</title>
+  <title><?php echo $username; ?> programmeerib veebi</title>
 
 </head>
 <body>
-  <h1>Harry Loog</h1>
+  <h1><?php echo $username; ?></h1>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli<a/> Digitehnoloogiate instituudis.<p>
+  <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
+  <p><?php echo "Praegu on " .$partofday ."."; ?></p>
 </body>
 </html>
