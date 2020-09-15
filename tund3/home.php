d713d5c92c6f860629347debc23d44226090d138
Harry Loog <applecat@users.noreply.github.com>
2020-09-15 10:12:07 +0300

    2345

--- tund3/home.php
+++ tund3/home.php
@@ -5,15 +5,17 @@
   $partofday = "lihtsalt aeg";
   $weekdaynameset = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
   $monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
-  var_dump($weekdaynameset;)
-  echo $weekdaynameset;
+#  var_dump($weekdaynameset);
+#  echo $weekdaynameset;
+	$weekdaynow = date("N");
+#	echo $weekdaynow;
   if($hournow <=6 and $hournow>0){
 	  $partofday = "öö";
   }
   if($hournow <12 and $hournow >6){
 	  $partofday = "hommik";
   }
-  if($hournow =12){
+  if($hournow ==12){
 	  $partofday = "keskpäev";
   }
   if($hournow >12 and $hournow<=16){
@@ -32,9 +34,32 @@
   $today = new DateTime("now");
   $dayselapsed = $semesterstart->diff($today);
   $dayselapseddays = $dayselapsed->format("%r%a");
-  if($dayselapseddays > 0 and $dayselapseddays < $semesterduration){
+  if($dayselapseddays >= 0 and $dayselapseddays <= $semesterduration){
 	  $dayselapsedpercentage = round($dayselapseddays/$semesterdurationdays*100 , 2);
 }
+	$picfiletypes = ["image/jpeg", "image/png"];
+	//loeme piltide kataloogi sisu ja näitame püilte
+	$allfiles = array_slice(scandir("../vp-pics/"), 2);
+	//$allfiles = scandir("../vp-pics/");
+	//var_dump($allfiles);
+	//$pic_files = array_slice($allfiles, 2);
+	$pic_files = [];
+	//var_dump($pic_files);
+	foreach($allfiles as $thing){
+	  $fileinfo = getImagesize("../vp-pics/" .$thing);
+	  if(in_array($fileinfo["mime"], $picfiletypes) == true){
+		  array_push($pic_files, $thing);
+	  }
+	}
+	$pic_count=count($pic_files);
+	//paneme kõik pildid ekraanile
+	$img_html = "";
+	for($i = 0; $i < $pic_count; $i ++){
+		$img_html .= '<img src="../vp-pics/' .$pic_files[$i] .'" ';
+		$img_html .= 'alt="Tallinna Ülikool"/>';
+	}//i=i+1 i=i++ i+=2
+	
+	
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -44,21 +69,32 @@
 
 </head>
 <body>
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner"/>
   <h1><?php echo $username; ?></h1>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli<a/> Digitehnoloogiate instituudis.<p>
-  <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
+  <p>Lehe avamise hetk: <?php echo $weekdaynameset[$weekdaynow-1] .", " .$fulltimenow; ?>.</p>
   <p><?php echo "Praegu on " .$partofday ."."; ?></p>
+  <p><?php echo $img_html;?></p>
   <?php
 		if($dayselapseddays > 0 and $dayselapseddays < $semesterduration){
-			echo "Semestri algusest on möödunud " .$dayselapseddays ." päeva.\n" ."Läbitud on " .$dayselapsedpercentage ."% semestrist!";
+			echo "Semester on käimas.\nOn semestri " .$dayselapseddays .". päev.\n" ."Semestrist on läbitud " .$dayselapsedpercentage ."%";
 		}
 		if($dayselapseddays <0){
-			echo "Semester pole veel alanud!\nLäbitud on 0% semestrist.";
+			echo "Semester pole veel alanud!";
 		}
 		if($dayselapseddays > $semesterduration){
-			echo "Semester on juba läbi.\nLäbitud on 100% semestrist.";
+			echo "Semester on juba läbi.";
+		}
+		if($dayselapseddays == 0){
+			echo "Semester algab täna\nSemestrist on läbitud " .$dayselapsedpercentage ."%";
+		}
+		if($dayselapseddays == $semesterdurationdays){
+			echo "Semester lõpeb täna\nSemestrist on läbitud " .$dayselapsedpercentage ."%";
 		}
 	?>
+<!--  
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner"/>
+-->
 </body>
 </html>
